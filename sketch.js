2784d8c162b994313f9db05a6de7c94517367a51
Sriram <sriramjj5@gmail.com>
2022-08-08 14:42:57 -0700

    Add support for custom grid size

--- sketch.js
+++ sketch.js
@@ -1,30 +1,39 @@
-for (let i = 0; i<16; i++) {
-    const el = document.createElement("div");
-    el.classList.add("vertcontainer");
-    for (let j = 0; j<16; j++) {
-        const horiz = document.createElement("div");
-        horiz.classList.add("horiz");
-        el.appendChild(horiz);
+const parent = document.getElementById("etchasketch");
+const button = document.getElementById("initialize");
+button.addEventListener("click", buttonPress);
+
+function buttonPress() {
+    let squares = prompt("How long/tall do you want your grid to be?");
+    initializeGrid(squares);
+}
+
+function initializeGrid(squares) {
+
+    if (parent.firstChild) {
+        myNode.removeChild(parent.firstChild);
     }
 
-    document.getElementById("body").appendChild(el);
+    for (let i = 0; i<squares; i++) {
+        const el = document.createElement("div");
+        el.classList.add("vertcontainer");
+        for (let j = 0; j<squares; j++) {
+            const horiz = document.createElement("div");
+            horiz.classList.add("horiz");
+            el.appendChild(horiz);
+        }
+
+        document.getElementById("etchasketch").appendChild(el);
+        
+    }
+
+    const divs = document.getElementsByClassName("horiz");
+ 
+    for (let i = 0; i < divs.length; i++) {
+        divs.item(i).addEventListener('mouseover', () => {divs.item(i).style.backgroundColor = 
+        'black';})
+        divs.item(i).style.height = screen.height/(1.25*squares) + "px"; 
+    }
+
+    parent.style.border = "5px solid red";
     
 }
-
-const divs = document.getElementsByClassName("horiz");
-
-for (let i = 0; i < divs.length; i++) {
-    divs.item(i).addEventListener('mouseover', () => {divs.item(i).style.backgroundColor = 
-    'black';})
-}
-
-
-// divs.forEach(addHoverEvent);
-
-// function addHoverEvent(div) {
-//     div.addEventListener('mouseover', handleMouseOver(div));
-// }
-
-// function handleMouseOver(div) {
-//     div.style.backgroundColor = 'black';
-// }
